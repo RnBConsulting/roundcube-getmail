5ba1ae4837a54908faebab92d41f32c74d1da6fa
Daniel Morlock <daniel.morlock@gmail.com>
2014-02-03 22:03:42 +0100

    Fix default value of "read_all" aka "only_new" ...

--- getmail_ui.php
+++ getmail_ui.php
@@ -139,7 +139,7 @@ class getmail_ui
         $field_id = 'config-only_new';
         $input = new html_checkbox(array('name' => 'only_new', 'id' => $field_id, 'value' => true));
         $table->add('title', html::label($field_id, $this->plugin->gettext('configonlynew')));
-        $table->add(null, $input->show($this->config ? $this->config['only_new'] : true));
+        $table->add(null, $input->show($this->config ? !$this->config['read_all'] : false));
 
         $field_id = 'config-poll';
         $input = new html_inputfield(array('name' => 'poll', 'id' => $field_id, 'size' => 10));
